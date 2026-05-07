895671a43af55a7043e5cc28b62bacdb8d806584
DhruvJaiswal-esc <dhruvphonewala@gmail.com>
2026-05-07 10:34:27 +0530

    Update App.jsx

--- ques2/src/App.jsx
+++ ques2/src/App.jsx
@@ -13,15 +13,15 @@ function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/">Home</Link> 
-        <Link to="/about">About</Link> 
-        <Link to="/contact">Contact</Link>
+        <Link to="/">Home</Link> {" "}
+        <Link to="/about">About</Link> {" "}
+        <Link to="/contact">Contact</Link>{" "}
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
       </Routes>
     </BrowserRouter>
   );
